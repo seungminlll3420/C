f884a5f78a5f89c13198a30effdb725a3dc96edb
seungminlll3420 <45160518+seungminlll3420@users.noreply.github.com>
2019-10-04 21:41:13 +0900

    update

--- hello/Project1.c
+++ hello/Project1.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #define MAX_N 15
-int weight[MAX_N+1]; // 학생 몸무게 데이터
-int result = 0; // 각 테스트 케이스에 대한 결과(경우의 수)
-int N = 0; // 학생 수
+int weight[MAX_N+1];
+int result = 0;
+int N = 0;
 void countTeams(int h, int wA, int wB, int wC) {
-// Base case: 모든 학생들을 팀에 넣은 후, A, B, C 팀의 몸무게가 같은 경우 result 1증가
-// h번째 학생을 팀 A/팀 B/팀 C/아무팀에도 안 넣는 경우(4가지)에 대해 재귀 호출
-  if(wA == wB == wC){
-    result++;
-  }
-  for(;k<N;h++){
+  if(h == N){
+    if((wA == wB)&(wB == wC)) result++;
+    return ;
+  }else{
+    h++;
     countTeams(h,wA,wB,wC);
+    countTeams(h,wA+weight[h],wB,wC);
+    countTeams(h,wA,wB+weight[h],wC);
+    countTeams(h,wA,wB,wC+weight[h]);
   }
 }
-
 int main() {
   int T;
-  freopen(“input.txt”, “r”, stdin);
+  int wA = 0, wB = 0, wC = 0;
+  freopen("input.txt", "r", stdin);
   scanf("%d", &T);
   for (int i = 0; i < T; i++) {
     scanf("%d", &N);
     for (int j = 1; j <= N; j++)
       scanf("%d", &weight[j]);
-// 각 테스트 케이스에 대한 경우의 수 구하고, 출력하기
+    wA =0; wB = 0; wC =0;
+    countTeams(0,wA,wB,wC);
+    printf("%d \n", result);
+    result = 0;
   }
 }
